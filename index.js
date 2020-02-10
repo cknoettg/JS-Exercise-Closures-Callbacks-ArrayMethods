0b40eb2e91a71844843cd5bd82f8da8086b20da8
Corey Knoettgen <cknoettg@yahoo.com>
2020-02-10 01:58:52 -0500

    Added getRunnersByTShirtSize

--- index.js
+++ index.js
@@ -227,8 +227,9 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
+function getRunnersByTShirtSize(runners, tShirtSize) {
   /* CODE HERE */
+  return runners.filter((state) => state.shirt_size === tShirtSize);
 }
 
 /**
