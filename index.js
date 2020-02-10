444448e60b38278172e5666e187674e90e2f1b56
Corey Knoettgen <cknoettg@yahoo.com>
2020-02-10 02:05:19 -0500

    Added tallyUpDonations

--- index.js
+++ index.js
@@ -242,8 +242,9 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
+function tallyUpDonations(runners) {
   /* CODE HERE */
+  return runners.reduce((total,state) => total += state.donation, 0);
 }
 
 /////////////// CLOSURES ///////////////
